8fc85fb5849097759b5b10ff1a94690c864a083c
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-06-18 15:31:56 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -10,18 +10,31 @@ int numberOfMinorColors = sizeof(minorColor) / sizeof(minorColor[0]);
 
 cableReferenceManual testCable_RefManualData_s[25];
 
-int printColorMap() {
-    int i = 0, j = 0, cablePairIndex = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            testCable_RefManualData_s[cablePairIndex].pairNo = i * 5 + j;
+int getColorMap() {
+    int majorColors_Index = 0, minorColors_Index = 0, cablePairIndex = 0;
+    
+    for(majorColors_Index = 0; majorColors_Index < numberOfMajorColors; majorColors_Index++) 
+    {
+        for(minorColors_Index = 0; minorColors_Index < numberOfMinorColors; minorColors_Index++) 
+        {
+            testCable_RefManualData_s[cablePairIndex].pairNo = majorColors_Index * 5 + minorColors_Index;
             testCable_RefManualData_s[cablePairIndex].majorColorCode = (enum MajorColor)i;
             testCable_RefManualData_s[cablePairIndex].minorColorCode = (enum MinorColor)i;
             ++cablePairIndex;
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
         }
     }
     return i * j;
+}
+
+void printColorMap()
+{
+    int majorColors_Index = 0, minorColors_Index = 0;
+    for(majorColors_Index = 0; majorColors_Index < numberOfMajorColors; majorColors_Index++) {
+        for(minorColors_Index = 0; minorColors_Index < numberOfMinorColors; minorColors_Index++) {
+    printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+        }
+    }
+    printf("All is well (maybe!)\n");
 }
 
 cableReferenceManual GetColorCodeFromPairNumber(int pairNumber) {
@@ -32,10 +45,6 @@ cableReferenceManual GetColorCodeFromPairNumber(int pairNumber) {
     return colorCode;
 }
 
-int GetPairNumberFromColorCode(const cableReferenceManual* colorCode) {
-    return colorCode->majorColorCode * numberOfMinorColors + colorCode->minorColorCode + 1;
-}
-
 void testMinor_CableColorCode(cableReferenceManual *const cableData)
 {
   cableReferenceManual colorCode_Data;
@@ -43,8 +52,8 @@ void testMinor_CableColorCode(cableReferenceManual *const cableData)
   if((cableData->pairNo > 0) && (cableData->pairNo <= 25))
   {
      colorCode_Data = GetColorCodeFromPairNumber(cableData->pairNo);
-     //assert(colorCode_Data.majorColorCode == cableData->majorColorCode); 
-     //assert(colorCode_Data.minorColorCode == cableData->minorColorCode); 
+    
+     assert(colorCode_Data.minorColorCode == cableData->minorColorCode); 
   }
   else if(cableData->pairNo == 0)
   {
@@ -53,14 +62,10 @@ void testMinor_CableColorCode(cableReferenceManual *const cableData)
 }
 
 int main() {
-    int cablePairIndex = 0;
-    int result = printColorMap();
-    //assert(result == 24);
-    
-    for(cablePairIndex = 0; cablePairIndex < result; cablePairIndex++)
-    {
+    int pairCount = getColorMap();
+    for(int cablePairIndex = 0; cablePairIndex < pairCount; cablePairIndex++){
         testMinor_CableColorCode(&testCable_RefManualData_s[cablePairIndex]); 
     }
-    printf("All is well (maybe!)\n");
+    printColorMap();
     return 0;
 }
