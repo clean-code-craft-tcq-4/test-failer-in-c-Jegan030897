4be47a993ab3df4d65eb43ed36fe6fc5e5656ef8
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-06-18 13:20:28 +0530

    Update cableColorCode_misaligned.h

--- cableColorCode_misaligned.h
+++ cableColorCode_misaligned.h
@@ -1,17 +1,8 @@
 #include "stdio.h"
 
-#define MAX_CABLE_COLOR_PAIR  25
-
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
 struct cableReferenceManual
 {
   int pairNo;
-  enum MajorColor majorColorCode;
-  enum MinorColor minorColorCode;
+  const char* majorColorCode;
+  const char* minorColorCode;
 };
-
-//void testCableColorCode(cableReferenceManual *const cabelData);
-cableReferenceManual GetColorCodeFromPairNumber(int pairNumber);
-int GetPairNumberFromColorCode(const cableReferenceManual* colorCode);
