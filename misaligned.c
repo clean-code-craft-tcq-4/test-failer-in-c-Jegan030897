9b304ad0551f79f0b3f886a061ef859f77502b90
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-06-18 11:37:43 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <assert.h>
+#include "cableColorCode_misaligned.h"
 
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
+    int i = 0, j = 0, cablePairIndex = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
+            testCable_RefManualData_s[cablePairIndex].pairNo = i * 5 + j;
+            testCable_RefManualData_s[cablePairIndex].majorColorCode = i;
+            testCable_RefManualData_s[cablePairIndex].minorColorCode = i;
+            
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
         }
     }
