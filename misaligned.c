076d957585f19bae34fb028615a7fdc3bf1046ec
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-06-18 13:42:05 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -2,11 +2,12 @@
 #include <assert.h>
 #include "cableColorCode_misaligned.h"
 
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
 cableReferenceManual testCable_RefManualData_s[25];
 
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0, cablePairIndex = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
@@ -20,27 +21,39 @@ int printColorMap() {
     return i * j;
 }
 
+cableReferenceManual GetColorCodeFromPairNumber(int pairNumber) {
+    cableReferenceManual colorCode;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorCode.majorColorCode = (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorCode.minorColorCode = (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorCode;
+}
+
+int GetPairNumberFromColorCode(const cableReferenceManual* colorCode) {
+    return colorCode->majorColorCode * numberOfMinorColors + colorCode->minorColorCode + 1;
+}
+
 void testCableColorCode(cableReferenceManual *const cabelData)
 {
-  int cablePairIndex = 0;
   cableReferenceManual colorCode_Data;
-  
-  for(cablePairIndex = 0; cablePairIndex < MAX_CABLE_COLOR_PAIR; cablePairIndex++)
+    
+  if((cabelData[cablePairIndex]->pairNo > 0) && (cabelData[cablePairIndex]->pairNo <= 25))
   {
-    if((cabelData[cablePairIndex]->pairNo > 0) && (cabelData[cablePairIndex]->pairNo <= MAX_CABLE_COLOR_PAIR))
-    {
-      colorCode_Data = GetColorCodeFromPairNumber(cabelData[cablePairIndex]->pairNo);
+     colorCode_Data = GetColorCodeFromPairNumber(cabelData[cablePairIndex]->pairNo);
       
-      assert(colorCode_Data.minorColorCode == cabelData[cablePairIndex]->minorColorCode);
-    }
+     assert(colorCode_Data.minorColorCode == cabelData[cablePairIndex]->minorColorCode);  
   }
-  
 }
 
 int main() {
+    int cablePairIndex = 0;
     int result = printColorMap();
     assert(result == 24);
-    testCableColorCode(testCable_RefManualData_s);
+    
+    for(cablePairIndex = 0; cablePairIndex < 25; cablePairIndex++)
+    {
+        testCableColorCode(&testCable_RefManualData_s[cablePairIndex]);
+    }
     printf("All is well (maybe!)\n");
     return 0;
 }
