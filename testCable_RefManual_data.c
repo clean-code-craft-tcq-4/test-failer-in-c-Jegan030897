fc5c27ccb1d85f66a1780104bc445bad47be9cd3
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-06-18 13:05:36 +0530

    Update testCable_RefManual_data.c

--- testCable_RefManual_data.c
+++ testCable_RefManual_data.c
@@ -14,20 +14,3 @@ cableReferenceManual GetColorCodeFromPairNumber(int pairNumber) {
 int GetPairNumberFromColorCode(const cableReferenceManual* colorCode) {
     return colorCode->majorColorCode * numberOfMinorColors + colorCode->minorColorCode + 1;
 }
-
-void testCableColorCode(cableReferenceManual *const cabelData)
-{
-  int cablePairIndex = 0;
-  cableReferenceManual colorCode_Data;
-  
-  for(cablePairIndex = 0; cablePairIndex < MAX_CABLE_COLOR_PAIR; cablePairIndex++)
-  {
-    if((cabelData[cablePairIndex]->pairNo > 0) && (cabelData[cablePairIndex]->pairNo <= MAX_CABLE_COLOR_PAIR))
-    {
-      colorCode_Data = GetColorCodeFromPairNumber(cabelData[cablePairIndex]->pairNo);
-      
-      assert(colorCode_Data.minorColorCode == cabelData[cablePairIndex]->minorColorCode);
-    }
-  }
-  
-}
